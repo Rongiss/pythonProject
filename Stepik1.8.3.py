d2ee7799daf8f8f7807487d0aea0cc4bff5c56d0
r.grigoriev <rongis03@gmail.com>
2021-02-01 15:26:06 +0800

    end

--- Stepik1.8.3.py
+++ Stepik1.8.3.py
@@ -1,6 +1,6 @@
 normal_to_sleep = int(input()) #time sleeping
 hour = int(input())            #hour going to sleep
-minutes = int(input())          #minut going to sleep
+minutes = int(input())         #minut going to sleep
 all_time = (normal_to_sleep + (hour * 60) + minutes)
 print((all_time // 60), (all_time % 60), sep="\n")
 
